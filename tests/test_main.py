de8387b3ad0c9c9d1602c4fac82346f475414844
Mithul Sudharsan <mithulsudharsanravikumar@Mithuls-MacBook-Pro.local>
2024-04-15 23:12:29 -0700

    first commit

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,9 @@
-
+import sys
 import unittest
+import pandas as pd  
+
+sys.path.append('../')
+
 from main import extract_hash, format_output, count_clicks
 
 class TestProjectFunctions(unittest.TestCase):
@@ -8,12 +12,12 @@ class TestProjectFunctions(unittest.TestCase):
         self.assertEqual(extract_hash("http://bit.ly/abc123"), "abc123")
 
     def test_format_output(self):
-        df_input = pd.DataFrame({'long_url': ['http://example.com', 'http://example.org'],
-                                 'count': [10, 5]})
+        df_input = pd.DataFrame({
+            'long_url': ['http://example.com', 'http://example.org'],
+            'count': [10, 5]
+        })
         expected_output = [{'http://example.com': 10}, {'http://example.org': 5}]
         self.assertEqual(format_output(df_input), expected_output)
-
-    # Add more tests as needed
 
 if __name__ == '__main__':
     unittest.main()
